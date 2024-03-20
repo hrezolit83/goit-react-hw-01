3b53ecdfbb2dc443151829099a64063a3665020b
hrezolit83 <deduh83@gmail.com>
2024-03-20 17:56:23 +0200

    modern-normalize

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/app/App.jsx";
 import "./index.css";
-// import "modern-normalize";
+import "modern-normalize";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
